00b17225ffd38f39953580393a3c20eca241f8b3
fhryazr <fahryazaria@gmail.com>
2025-12-12 01:17:46 +0800

    feat : add view all button on recent links card

--- src/features/shorten-link/components/RecentLinksCard.tsx
+++ src/features/shorten-link/components/RecentLinksCard.tsx
@@ -9,33 +9,8 @@ import {
 } from "@/components/ui/card";
 import ShortenLinkCardList from "./ShortenLinkCardList";
 import { useGetShortenLinks } from "@/api/shorten/getShorten";
-
-// const data: ShortenLink[] = [
-//   {
-//     id: 1,
-//     shortLink: "short.ly/abc123",
-//     originalLink: "https://example.com/very/long/url/1",
-//     shortCode: "abc123",
-//     createdAt: "2024-06-01T12:00:00Z",
-//     updatedAt: "2024-06-01T12:00:00Z",
-//   },
-//   {
-//     id: 2,
-//     shortLink: "short.ly/def456",
-//     shortCode: "def456",
-//     originalLink: "https://example.com/very/long/url/2",
-//     createdAt: "2024-06-01T12:00:00Z",
-//     updatedAt: "2024-06-01T12:00:00Z",
-//   },
-//   {
-//     id: 3,
-//     shortLink: "short.ly/ghi789",
-//     shortCode: "ghi789",
-//     originalLink: "https://example.com/very/long/url/3",
-//     createdAt: "2024-06-01T12:00:00Z",
-//     updatedAt: "2024-06-01T12:00:00Z",
-//   },
-// ];
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const RecentLinksCard = () => {
   const { data: links, isLoading: fetchLinksLoading } =
@@ -43,11 +18,16 @@ const RecentLinksCard = () => {
 
   return (
     <Card>
-      <CardHeader>
-        <CardTitle>Recent Links</CardTitle>
-        <CardDescription>
-          Your most recently created short links
-        </CardDescription>
+      <CardHeader className="flex justify-between">
+        <div>
+          <CardTitle>Recent Links</CardTitle>
+          <CardDescription>
+            Your most recently created short links
+          </CardDescription>
+        </div>
+        <Button variant="outline" className="bg-white">
+          <Link href="/dashboard/my-links">View All</Link>
+        </Button>
       </CardHeader>
       <CardContent>
         <ShortenLinkCardList
